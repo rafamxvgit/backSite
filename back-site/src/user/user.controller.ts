782ed944c7376e448ec75fb594e650c805e7fc9d
Rafael Monteiro <rmxvmail@gmail.com>
2024-05-20 21:27:24 -0300

    remoção de mais coisa inútil

--- back-site/src/user/user.controller.ts
+++ back-site/src/user/user.controller.ts
@@ -4,7 +4,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  
+	constructor(private readonly userService: UserService) {}
 
   	@Post()
   	async create(@Body() createUserDto: CreateUserDto) {
